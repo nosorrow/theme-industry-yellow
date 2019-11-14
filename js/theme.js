c5f9e225b0171fc470149f257ad1cc0ce08cb64d
nosorrow <plamenorama@gmail.com>
2019-11-14 20:05:00 +0200

    14.11.2019

--- js/theme.js
+++ js/theme.js
@@ -1,8 +1,44 @@
-var app = {
+/* Fix top navbar on scrolldown */
+const topContacts = document.getElementById('top-contacts');
+const header = document.getElementById('hero');
+const headerHeight = header.clientHeight;
+const mainmenu = document.getElementById('main-menu');
+
+/* Navbar hover */
+const $dropdown = $(".navbar-nav li");
+const $submenu  = $(".dropdown-submenu");
+const $dropdownToggle = $(".dropdown-toggle");
+const $dropdownMenu = $(".dropdown-menu:not(.dropdown-submenu)");
+const showClass = "show";
+
+var theme = {
+    // Set default options
+    dropdownHover: false,
+    navbarFixed: false,
+    equalize: [
+        '.byt-card-news',
+        '.carousel .carousel-item'
+    ],
+    settings: function (options) {
+        console.log(this.navbarFixed);
+        this.dropdownHover = options.dropdownHover || this.dropdownHover;
+        this.equalize = options.equalize || this.equalize;
+        this.navbarFixed = options.navbarFixed || this.navbarFixed;
+        console.log(this.navbarFixed);
+        return this;
+    },
+    run: function () {
+        app.equalize(this.equalize);
+        app.fixNavbar(this.navbarFixed);
+        app.onHoverNav(this.dropdownHover)
+    }
+};
+
+const app = {
 
     // All selected elements have some height
     equalize: function (elements) {
-        elements.element.map(this.equizer);
+        elements.map(this.equizer);
     },
     equizer: function (select) {
         const selector = document.querySelectorAll(select);
@@ -15,67 +51,61 @@ var app = {
             }
             e.style.height = Yheight + "px";
         });
+    },
+    // Fix top navbar on scroll
+    fixNavbar: function (opt) {
+        if (opt === true){
+            window.addEventListener('scroll', function (e) {
+
+                if(window.scrollY > header.clientHeight + 45){
+                    topContacts.classList.add("change-top-cont");
+                    mainmenu.classList.add("fixed-top", "changednav", "slideInDown", "animated");
+                } else {
+                    topContacts.classList.remove("change-top-cont");
+                    mainmenu.classList.remove("fixed-top", "changednav", "slideInDown", "animated");
+                }
+
+            });
+        }
+    },
+    // Dropdown hover
+    onHoverNav: function (opt){
+    if (opt === true){
+        $(window).on("load resize", function() {
+            if (this.matchMedia("(min-width: 768px)").matches) {
+                $dropdown.hover(
+                    function() {
+                        const $this = $(this);
+                        $this.addClass(showClass);
+                        $this.find($dropdownToggle).attr("aria-expanded", "true");
+                        // $this.find($dropdownMenu).addClass(showClass).fadeIn('slow');
+                        $this.find($dropdownMenu).fadeIn();
+                    },
+                    function() {
+                        const $this = $(this);
+                        $this.removeClass(showClass);
+                        $this.find($dropdownToggle).attr("aria-expanded", "false");
+                        // $this.find($dropdownMenu).removeClass(showClass);
+                        $this.find($dropdownMenu).fadeOut(10);
+                    }
+                );
+
+                $("div.submenu").hover(
+                    function() {
+                        $(this).find($submenu).fadeIn()
+                    },
+                    function() {
+                        $(this).find($submenu).fadeOut();
+                    }
+                )
+            } else {
+                $dropdown.off("mouseenter mouseleave");
+            }
+        });
+
     }
+}
 };
-
-/* Fix top navbar on scrolldown */
-const topContacts = document.getElementById('top-contacts');
-const header = document.getElementById('hero');
-const headerHeight = header.clientHeight;
-const mainmenu = document.getElementById('main-menu');
-
-window.addEventListener('scroll', function (e) {
-
-    if(window.scrollY > header.clientHeight + 45){
-        topContacts.classList.add("change-top-cont");
-        mainmenu.classList.add("fixed-top", "changednav", "slideInDown", "animated");
-    } else {
-        topContacts.classList.remove("change-top-cont");
-        mainmenu.classList.remove("fixed-top", "changednav", "slideInDown", "animated");
-    }
-
-});
-
-/* Navbar hover */
-const $dropdown = $(".navbar-nav li");
-const $submenu  = $(".dropdown-submenu");
-const $dropdownToggle = $(".dropdown-toggle");
-const $dropdownMenu = $(".dropdown-menu:not(.dropdown-submenu)");
-const showClass = "show";
-/*
-
-$(window).on("load resize", function() {
-    if (this.matchMedia("(min-width: 768px)").matches) {
-        $dropdown.hover(
-            function() {
-                const $this = $(this);
-                $this.addClass(showClass);
-                $this.find($dropdownToggle).attr("aria-expanded", "true");
-               // $this.find($dropdownMenu).addClass(showClass).fadeIn('slow');
-                $this.find($dropdownMenu).fadeIn();
-            },
-            function() {
-                const $this = $(this);
-                $this.removeClass(showClass);
-                $this.find($dropdownToggle).attr("aria-expanded", "false");
-                // $this.find($dropdownMenu).removeClass(showClass);
-                $this.find($dropdownMenu).fadeOut(10);
-            }
-        );
-
-        $("div.submenu").hover(
-            function() {
-                $(this).find($submenu).fadeIn()
-            },
-            function() {
-                $(this).find($submenu).fadeOut();
-            }
-        )
-    } else {
-        $dropdown.off("mouseenter mouseleave");
-    }
-});
-*/
 
 
 // Jquery equal height
